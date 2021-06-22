04974384480589fb8fe257988d445055b8b8fa16
Felix De la Cruz <felixtoledo95@gmail.com>
2021-06-22 07:11:20 -0500

    Delete unnecessary import

--- client/src/reducers/CombinedReducers.js
+++ client/src/reducers/CombinedReducers.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-
-import { me } from './Me';
 import { people } from './peopleReducer';
 
 const Reducers = combineReducers({
