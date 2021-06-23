1f81916ec7b6346cad2074e410e773a2692ab63d
Felix De la Cruz <felixtoledo95@gmail.com>
2021-06-22 23:59:44 -0500

    let this repo just for front end

--- app/app.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({path: 'config/.env'});
-const Server = require('./services/server');
-
-const server = new Server();
-
-server.listen();
--- client/src/actions/AsyncActions.js
+++ client/src/actions/AsyncActions.js
@@ -4,7 +4,7 @@ export const creds = { credentials: 'same-origin' };
 
 export function fetchPeopleList() {
   return dispatch => {
-    return fetch('http://localhost:5000/api', creds)
+    return fetch('https://salesloft-app-felix-delacruz.herokuapp.com/api', creds)
       .then(response => response.json())
       .then(peopleList => dispatch(actions.receivePeopleList(peopleList)))
       .catch((error) => console.log(error));
@@ -13,7 +13,7 @@ export function fetchPeopleList() {
 
 export function fetchUniqueEmailsCharacters() {
   return dispatch => {
-    return fetch('http://localhost:5000/api/charactersCount', creds)
+    return fetch('https://salesloft-app-felix-delacruz.herokuapp.com/api/charactersCount', creds)
       .then(response => response.json())
       .then(uniqueCharacters => dispatch(actions.receiveUniqueEmailsCharacters(uniqueCharacters)))
       .catch((error) => console.log(error));
@@ -22,7 +22,7 @@ export function fetchUniqueEmailsCharacters() {
 
 export function fetchPossibleEmailsDuplicates() {
   return dispatch => {
-    return fetch('http://localhost:5000/api/possibleDuplicates', creds)
+    return fetch('https://salesloft-app-felix-delacruz.herokuapp.com/api/possibleDuplicates', creds)
       .then(response => response.json())
       .then(possibleDuplicates => dispatch(actions.receivePossibleEmailsDuplicate(possibleDuplicates)))
       .catch((error) => console.log(error));
